8f0653e8cf688aeb074677a31ca1b0c2eba98de0
AlkaTiwary <125354855+AlkaTiwary@users.noreply.github.com>
2024-02-26 16:01:28 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 3.0"
+        }
+    }
+}
+provider "aws" {
+region = "us-east-1"
+access_key = "ASIA2OCCQSOE5BICUDOQ"
+secret_key = "IOEwXlvZkRMwfGNzEOrZIgytTydP413N51boEL2H"
+token ="FwoGZXIvYXdzEOP//////////wEaDHNiiS+8rjIRcQ3UfyKvAb5ShVgRszszOsNu4aPbL6tIVrR+ew/9XoP3DAjbvX0h6lchmzfra+NJAZjX7TarPM67zXucu9bwQk7N3kUExfEvdR8plrfd0Y/721JNpgaPEi5vldYj8XdwQewg/Y1CLVyvb0k55gLNWQFAfXO/KDkFXEbzHuhVWUtomeQEAC/8vwfD15ZJGNybowxn/RIwoN5uacSSe9M8lIy/BVDzRMtwUxUdsW5Q3QliugVAMb4oobvxrgYyLdcDDkgY5SzHfsSFFELNnUimk4MhZtlGfQGDE105DaFJMW+cMCAD+UmwsgJ+gw=="
+}
